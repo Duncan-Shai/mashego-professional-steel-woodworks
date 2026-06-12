f4beb896bb757be746b994082285c7fc8a7bdcfe
Duncan-Shai <mabusashai@gmail.com>
2026-06-12 23:51:45 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,16 @@ const cors = require("cors");
 
 const app = express();
 
+const quoteRoutes = require("./routes/quotes");
+
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Mashego Professional Steel and Woodworks backend is running");
 });
+
+app.use("/quotes", quoteRoutes);
 
 const PORT = 3000;
 
